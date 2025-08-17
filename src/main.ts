260d9e02c8ca45c5549dfcd03fba79c30fc7a0f3
Ruskiy Negro <131805484+luckyjokela@users.noreply.github.com>
2025-08-17 18:58:36 +0300

    sm update

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,19 @@ import { AppPostgreSQLDataSource } from './infrastructure/persistence/typeorm/da
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
+    await AppPostgreSQLDataSource.initialize();
+    console.log('✅ Database connected');
     const configService = app.get(ConfigService);
+    app.enableCors({
+      origin: `${configService.get<string>('HOST')}${configService.get<number>('PORT')}`,
+      credentials: true,
+    });
     const port = configService.get<number>('PORT', 3002);
     await app.listen(port);
   } catch (error) {
     console.error('🚫 Ошибка при запуске сервера:', error);
     throw error;
   }
-  await AppPostgreSQLDataSource.initialize();
-  console.log('✅ Database connected');
 }
 
 void bootstrap().catch((err) =>
